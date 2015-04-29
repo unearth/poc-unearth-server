81356aeb6030158504ab48c75b930f82e9539bcc
Sean Conroy <conroysean@gmail.com>
2015-04-29 12:34:56 -0700

    [feat] Adds auth routes

---
+++ server/routers/authenticationRouter.js
@@ -0,0 +1,17 @@
+module.exports = function(app){
+  app.get('/login', function(req, res){
+    res.send('Received login get request! Sending login page.');
+  });
+
+  app.post('/login', function(req, res){
+    res.send('Received login post request! Initiating login procedure.');
+  });
+
+  app.get('/signup', function(req, res){
+    res.send('Received signup get request! Sending signup page.');
+  });
+
+  app.post('/signup', function(req, res){
+    res.send('Received signup post request! Initiating signup procedure.');
+  });
+};
--- server/routers/waypointRouter.js
+++ server/routers/waypointRouter.js
@@ -1,5 +1,8 @@
 module.exports = function(app) {
   app.get('/', function(req, res){
-    res.send('Received get request!');
+    res.send('Received waypoint get request! Sending applicable waypoints.');
+  });
+  app.post('/', function(req, res){
+    res.send('Received waypoint post request! Applying applicable waypoints.');
   });
 };
