7707c181092ef2f169abc6d9e82094a023e31f1c
Sean Conroy <conroysean@gmail.com>
2015-04-29 12:35:20 -0700

    [feat] Adds auth routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 var express = require('express'),
     app = express(),
-    waypointRouter = express.Router();
+    waypointRouter = express.Router(),
+    authenticationRouter = express.Router();
+
 
 require('./routers/waypointRouter')(waypointRouter);
+require('./routers/authenticationRouter')(authenticationRouter);
+
 
 // requests to /waypoint/* will be sent to this router
 
 app.use('/waypoint', waypointRouter);
+app.use('/user', authenticationRouter);
 
 module.exports = app;
