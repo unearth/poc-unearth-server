5192fe2b84038a3029fd82734239b680ea1f0404
ivebencrazy <ben@blanketwarriors.com>
2015-04-30 12:11:47 -0700

    [feature] Adds structure for Passport auth  - Is not functional

--- server/app.js
+++ server/app.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var app = express();
+
+// Dependencies
+var morgan = require('morgan');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+app.use(morgan('dev'));  // Log to the console
+app.use(cookieParser()); // Read cookies for auth
+
+// Parse post bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(session({
+  secret: 'supersecretsecret',
+  resave: true,            // https://github.com/expressjs/session#resave
+  saveUninitialized: true // https://github.com/expressjs/session#saveuninitialized
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // Persistent login sessions
+app.use(flash()); // For flash messages stored in session
+
+// Routers
 var waypointRouter = express.Router();
 var authenticationRouter = express.Router();
 
-require('./routers/waypointRouter')(waypointRouter);
-require('./routers/authenticationRouter')(authenticationRouter);
+require('./routers/authenticationRouter')(authenticationRouter, passport);
 
-// Requests to /waypoint/* will be sent to this router
-app.use('/waypoint', waypointRouter);
 app.use('/user', authenticationRouter);
 
 // Catches 404 and forwards to error handler
-app.use(function(request, response, next) {
+app.use(function(request, response) {
   var error = new Error('Not Found');
   response.status(404).send(error);
 });
--- server/routers/authenticationRouter.js
+++ server/routers/authenticationRouter.js
@@ -1,9 +1,34 @@
-module.exports = function(app) {
+module.exports = function(app, passport) {
   app.post('/login', function(request, response) {
-    response.send('Received login post request! Initiating login procedure/send back token.');
+    // Do Passport Stuff
   });
 
   app.post('/signup', function(request, response) {
-    response.send('Received signup post request! Initiating signup procedure/send back token.');
+    // Do Passport Stuff
+  });
+
+  app.post('/profile', isLoggedIn, function(request, response) {
+    // Send profile data
+    request.send(profile);
+  });
+
+  app.get('/waypoints', isLoggedIn, function(request, response) {
+    // Get waypoints
+    response.send('waypoints');
+  });
+  app.post('/waypoints', isLoggedIn, function(request, response) {
+    // Add a waypoint
+  });
+
+  app.get('/logout', function(request, response) {
+    request.logout();
   });
 };
+
+// Routes middleware to make sure a user is logged in
+function isLoggedIn(request, response, next) {
+  // If user is authenticated in the session, carry on
+  if (request.isAuthenticated()){
+      return next();
+  }
+}
