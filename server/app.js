7128525fb6256e9ef1cce791f2e16b45597fd043
Sean Conroy <conroysean@gmail.com>
2015-04-29 19:43:15 -0700

    [feat] Adds some testing, TODO: debug post tests, supertest

--- server/app.js
+++ server/app.js
@@ -13,4 +13,12 @@ require('./routers/authenticationRouter')(authenticationRouter);
 app.use('/waypoint', waypointRouter);
 app.use('/user', authenticationRouter);
 
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 module.exports = app;
