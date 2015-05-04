e83aac781fbad206d8c858ddf04a66e689fdb779
ivebencrazy <ben@blanketwarriors.com>
2015-05-04 15:51:12 -0700

    [doc] Adds additional documentation and adjusts naming conventions to match the repo  - Renamed contribution_guide to match README and STYLE_GUIDE  - Small gruntfile changes to reflect moving directories  - Adds package.json dependencies for server/database  - Creates config.example.js as an example of how to import keys/servernames/etc

--- server/config.example.js
+++ server/config.example.js
@@ -1,5 +1,22 @@
 module.exports = {
   "db": {
     "databaseUrl": "postgres://username:@localhost:5432/unearth"
+  },
+  'facebookAuth' : {
+    'clientId'      : 'your-secret-clientID-here', // your App ID
+    'clientSecret'  : 'your-client-secret-here', // your App Secret
+    'callbackUrl'   : 'http://localhost:8080/auth/facebook/callback'
+  },
+
+  'twitterAuth' : {
+    'consumerKey'       : 'your-consumer-key-here',
+    'consumerSecret'    : 'your-client-secret-here',
+    'callbackUrl'       : 'http://localhost:8080/auth/twitter/callback'
+  },
+
+  'googleAuth' : {
+    'clientId'      : 'your-secret-clientID-here',
+    'clientSecret'  : 'your-client-secret-here',
+    'callbackUrl'   : 'http://localhost:8080/auth/google/callback'
   }
 };
