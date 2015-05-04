849277e0054de9eabf87a89364ff859876813656
ivebencrazy <ben@blanketwarriors.com>
2015-05-04 16:14:10 -0700

    [feature] Adds basic authorization  - Does NOT use tokens  - Must receive a email/password with every request  - Blocks users from getting waypoints without a valid email/password

---
+++ server/authorization/authStrategies.js
@@ -0,0 +1,26 @@
+var passport = require('passport');
+var util = require('./authUtils');
+var dbHelpers = require('../database/dbHelpers');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use('basic-login', new BasicStrategy(
+  function(email, password, done) {
+    dbHelpers.getUser(email, function(error, user){
+
+      if (error) { return done(error); }
+
+      // No user found with that email
+      if (!user) { return done(null, false); }
+
+      // Make sure the password is correct
+      if( !util.validPassword(password, user.password) ){
+        return done(null, false);
+      }
+
+      // Success
+      return done(null, user);
+    });
+  }
+));
+
+exports.isAuthenticated = passport.authenticate('basic-login', { session : false });
--- server/authorization/authUtils.js
+++ server/authorization/authUtils.js
@@ -1,27 +1,9 @@
-var bcrypt   = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt-nodejs');
 
-// Generates a hash
-exports.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+module.exports.hash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-// Checks if password is valid
-exports.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-exports.methods.validateEmail = function(email) {
-  // Search user by email
-  db.getLocalUser(email, function(error, rows) {
-    if(error){
-      return done(error);
-    }
-
-    if(rows.length > 0){
-      // Auth doesn't pass!
-    } else{
-      // Auth passes!
-      db.insertLocalUser(/*local.email, generateHash(password)*/);
-    }
-  });
+module.exports.validPassword = function(newPassword, storedPassword) {
+  return bcrypt.compareSync(newPassword, storedPassword);
 };
