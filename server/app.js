a29f64be86992cbfab4fcc3d3f02c7ee156b3e09
travisjn <tdizzletrav@gmail.com>
2015-05-07 15:18:43 -0700

    [feature] Allows CORS requests

--- server/app.js
+++ server/app.js
@@ -19,6 +19,12 @@ app.use(bodyParser.json());
 // Initializes passport
 app.use(passport.initialize());
 
+// Allows CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 ////////////////////////
 // ROUTES
