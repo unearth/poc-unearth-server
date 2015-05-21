d2b0284fd7bcac57e133f3cdd9b583217c48d3fe
Sean Conroy <conroysean@gmail.com>
2015-05-21 16:18:51 -0700

    [feat] marker-images support

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,4 +1,15 @@
 var dbHelpers = require('../database/dbHelpers.js');
+var cloudAPI = require('../config.js');
+var cloudinary = require('cloudinary');
+var multiparty = require('multiparty');
+var fs = require('fs');
+
+
+cloudinary.config({
+  cloud_name: cloudAPI.cloudinary.cloudName,
+  api_key: cloudAPI.cloudinary.apiKey,
+  api_secret: cloudAPI.cloudinary.apiSecret
+});
 
 module.exports = function(app, authController) {
 
@@ -27,4 +38,32 @@ module.exports = function(app, authController) {
       response.status(200).json({success: 'Session has been removed!'});
     });
   });
-};
+
+  app.post('/imageUpload', function(req, res) {
+
+    var form = new multiparty.Form();
+
+    var resizeImageURL = function(url, width) {
+      var urlPieces = url.split('upload/');
+      return urlPieces[0] + 'upload/w_'+width+'/' + urlPieces[1];
+    }
+
+    var stream =  cloudinary.uploader.upload_stream(function(result) {
+      // Get the image_url for a 170px wide image
+      req.body.image_url = resizeImageURL(result.url, 170);
+      // Post the review with req containing the necessary properties
+      res.status(202).end(JSON.stringify(req.body));
+    });
+      // The form parses the req, and finds the fields and files
+    form.parse(req, function(err, fields, files){
+      // Add the fields from the FormData to the req body
+      for (var key in fields) {
+        req.body[key] = fields[key][0];
+      }
+      // Send the file path to the cloudinary stream reader by piping from a fs.createReadStream
+      fs.createReadStream(files.file[0].path)
+        .pipe(stream);
+    })
+  })
+
+}
