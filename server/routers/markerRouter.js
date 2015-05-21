77e73388cb72f1399cc4ddb89b7982fa13d9d2f3
Sean Conroy <conroysean@gmail.com>
2015-05-21 16:47:10 -0700

    [debug] Streamlines naming conventions, removes unnecessary require statement

--- server/routers/markerRouter.js
+++ server/routers/markerRouter.js
@@ -1,5 +1,5 @@
 var dbHelpers = require('../database/dbHelpers.js');
-var markerHelpers = require('../database/markerHelpers.js');
+var markerHelpers = require('../database/dbmarkerHelpers.js');
 var cloudAPI = require('../config.js');
 var cloudinary = require('cloudinary');
 var multiparty = require('multiparty');
@@ -52,7 +52,7 @@ module.exports = function(app, authController) {
     });
   });
 
-  app.post('/imageUpload', function(req, res) {
+  app.post('/image', function(request, response) {
 
     var form = new multiparty.Form();
 
@@ -63,15 +63,15 @@ module.exports = function(app, authController) {
 
     var stream =  cloudinary.uploader.upload_stream(function(result) {
       // Get the image_url for a 170px wide image
-      req.body.image_url = resizeImageURL(result.url, 170);
+      request.body.image_url = resizeImageURL(result.url, 170);
       // Post the review with req containing the necessary properties
-      res.status(202).end(JSON.stringify(req.body));
+      response.status(202).end(JSON.stringify(request.body));
     });
       // The form parses the req, and finds the fields and files
-    form.parse(req, function(err, fields, files){
+    form.parse(request, function(err, fields, files){
       // Add the fields from the FormData to the req body
       for (var key in fields) {
-        req.body[key] = fields[key][0];
+        request.body[key] = fields[key][0];
       }
       // Send the file path to the cloudinary stream reader by piping from a fs.createReadStream
       fs.createReadStream(files.file[0].path)
